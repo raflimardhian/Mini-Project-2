745dc5e2d439c186005a2aac8874a1554a134d10
raflimardhian <raflimardhian02@gmail.com>
2026-02-04 11:49:11 +0700

    chore: change redis config file for deployment

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -1,23 +1,40 @@
 const { createClient } = require("redis");
 // const logger = require("./logger");
 
-const client = createClient();
+const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379';
+const client = createClient({
+    url: REDIS_URL,
+    socket: {
+        tls:true,
+        connectTimeout:10000,
+        reconnectStrategy: (retries) => {
+            if (retries > 10) return new Error("Redis reconnection failed");
+            return Math.min(retries * 50, 2000);
+        }
+    }
+});
 
 client.on("error", (err) => {
     console.log("Redis Client Error", err);
 });
 
-(async () => {
-    try {
-        await client.connect();
-        console.log("Connected to Redis");
-        
-        // logger.info("Connected to Redis");
-    } catch (err) {
-        console.log("Could not connect to Redis", err);
-        
-        // logger.error("Could not connect to Redis", err);
+let isConnected = false;
+
+const connnectRedis = async () => {
+    if(!isConnected){
+        try {
+            await client.connect();
+            console.log("Connected to Redis");
+            
+            // logger.info("Connected to Redis");
+        } catch (err) {
+            console.log("Could not connect to Redis", err);
+            
+            // logger.error("Could not connect to Redis", err);
+        }
     }
-})();
+    return client
+};
+connnectRedis()
 
 module.exports = client;
